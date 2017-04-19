402eb0a0880ee1b901362e8ad39b4dd0b7e8f60a
Leonard Grazian <grazian.leonard@gmail.com>
2017-04-19 14:27:15 -0400

    most recent run

--- 1D/1D_Ising.py
+++ 1D/1D_Ising.py
@@ -17,14 +17,14 @@ import time
 
 ### PARAMETERS ###
 
-N = 10 #0
+N = 50 # 100 #0
 M = N
 T = 300 # Kelvin
 BETA = 1 # (1.0 / (1.38064852 * T)) * math.pow(10, 23) # find how to set this
 J = 1 # e-20 # everything is normalized w.r.t. this, set to 1
 H = 0 # = B * mu
 
-MCS = 10000 #00 # number of Monte Carlo steps
+MCS = 10000000 # number of Monte Carlo steps
 
 dim1 = False
 
@@ -32,7 +32,7 @@ dim1 = False
 ### FUNCTIONS ###
 
 def update(ising_array):
-	if len(ising_array.shape) == 1: # 1D
+	if dim1: # 1D
 		flip_index = random.randint(0, N - 1)
 	
 		# get delta_E
@@ -63,33 +63,6 @@ def update(ising_array):
 		for index in neighbors:
 			index_x, index_y = index 
 			delta_E += -J * ising_array[index_x, index_y] * candidate
-#		if flip_index_x > 0:
-#			left_neighbor = ising_array[flip_index_x - 1, flip_index_y]
-#			delta_E += -J * left_neighbor * candidate
-#			
-#			if flip_index_y > 0:
-#				bottom_neighbor = ising_array[flip_index_x, flip_index_y - 1]
-#				bottom_left_neighbor = ising_array[flip_index_x - 1, flip_index_y - 1]
-#				delta_E += -J * (bottom_left_neighbor + bottom_neighbor) * candidate
-#				
-#			if flip_index_y < M - 1:
-#				top_neighbor = ising_array[flip_index_x, flip_index_y + 1]
-#				top_left_neighbor = ising_array[flip_index_x - 1, flip_index_y + 1]
-#				delta_E += -J * (top_left_neighbor + top_neighbor) * candidate
-#				
-#		if flip_index_x < N - 1:
-#			right_neighbor = ising_array[flip_index_x + 1, flip_index_y]
-#			delta_E += -J * right_neighbor * candidate
-#			
-#			if flip_index_y > 0:
-#				bottom_neighbor = ising_array[flip_index_x, flip_index_y - 1]
-#				bottom_right_neighbor = ising_array[flip_index_x + 1, flip_index_y - 1]
-#				delta_E += -J * (bottom_right_neighbor + bottom_neighbor) * candidate
-#			
-#			if flip_index_y < M - 1:
-#				top_neighbor = ising_array[flip_index_x, flip_index_y + 1]
-#				top_right_neighbor = ising_array[flip_index_x + 1, flip_index_y + 1]
-#				delta_E += -J * (top_right_neighbor + top_neighbor) * candidate
 				
 		
 		if delta_E < 0 or random.random() < math.exp(-BETA * delta_E): # update ising_array
@@ -99,7 +72,7 @@ def update(ising_array):
 			return 0
 
 def get_energy(ising_array):
-	if len(ising_array.shape) == 1: # 1D
+	if dim1: # 1D
 		energy_accumulator = 0
 		last_element = 0
 		for current_element in ising_array:
@@ -160,10 +133,13 @@ def get_spec_heat(energy_list):
 ### MAIN ###
 
 spec_heat_list = []
-for BETA in np.arange(.2, 1, 0.01): #0.005):
+beta_vals = np.arange(.2, 1, 0.05)
+for BETA in beta_vals: #0.005):
 #	if BETA % 10 == 0:
 #		print 'Working on BETA = ' + str(BETA)
 		
+	print 'working on BETA = ' + str(BETA)
+	
 	if dim1:
 		ising_array = np.zeros(N, dtype='int32')
 		for i in range(N):
@@ -177,6 +153,7 @@ for BETA in np.arange(.2, 1, 0.01): #0.005):
 
 	energy_list = []
 	current_energy = get_energy(ising_array)
+#	current_mag = get_mag(ising_array)
 	is_in_equilibrium = False
 	for i in range(MCS):
 #		flip_index = random.randint(0, N - 1)
@@ -213,5 +190,5 @@ for BETA in np.arange(.2, 1, 0.01): #0.005):
 	spec_heat_list.append(spec_heat)
 
 
-plt.plot(np.arange(.2, 1, 0.01), spec_heat_list)
+plt.plot(beta_vals, spec_heat_list)
 plt.show()
